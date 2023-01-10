da03e45917586372b9bcd1e800089124279b1aff
Darkhan Kubigenov <darkhanu@gmail.com>
2023-01-10 02:14:19 +0000

    Define a layer without an escape key

--- keyboards/annepro2/keymaps/kdarkhan/config.h
+++ keyboards/annepro2/keymaps/kdarkhan/config.h
@@ -3,3 +3,5 @@
 #define UCIS_MAX_CODE_POINTS 12
 
 #define RGBLIGHT_SLEEP yes
+
+#define TAP_CODE_DELAY 10
--- keyboards/annepro2/keymaps/kdarkhan/keymap.c
+++ keyboards/annepro2/keymaps/kdarkhan/keymap.c
@@ -21,6 +21,7 @@
 enum anne_pro_layers {
   _BASE_LAYER,
   _WIFE_LAYER,
+  _NO_ESC_LAYER,
   _CAPS_LAYER,
   _FN1_LAYER,
   _FN2_LAYER,
@@ -33,6 +34,7 @@ enum custom_codes {
   KC_AP_LIN,
   KC_AP_MAC,
   KC_AP_WIFE,
+  KC_AP_NO_ESC,
   KC_AP_RGB_SPEED,
 };
 
@@ -81,6 +83,12 @@ enum tap_dance_codes {
         LT(_CAPS_LAYER, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP,
         KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, TT(_FN1_LAYER), KC_LEFT, KC_DOWN, KC_RIGHT),
+    [_NO_ESC_LAYER]  = LAYOUT_60_ansi(/* Base */
+        KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, LT(_TAP2_LAYER, KC_BSLS),
+        TT(_CAPS_LAYER), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, TT(_FN1_LAYER), LT(_FN2_LAYER, KC_APP), KC_RALT, TD(KC_AP_TD_RCTRL)),
     [_CAPS_LAYER] = LAYOUT_60_ansi(/* Hold CapsLock */
         _______, _______, _______, _______, _______, KC_AUDIO_VOL_DOWN, KC_AUDIO_MUTE, KC_AUDIO_VOL_UP, KC_BRID, KC_BRIU, _______, _______, _______, KC_DELETE,
         _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, KC_HOME, KC_END, _______,
@@ -100,7 +108,7 @@ enum tap_dance_codes {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_VAI,
         _______, _______, _______, _______, TT(_FNX_LAYER), _______, _______, KC_AP_RGB_SPEED),
     [_TAP2_LAYER]   = LAYOUT_60_ansi(/* Hold |\ */
-        _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______,
+        KC_ESC,  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______,
         _______, KC_MS_BTN1, KC_MS_UP, KC_MS_BTN2, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         KC_CAPS, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, KC_MS_WH_DOWN, _______, KC_MS_WH_UP, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -108,7 +116,7 @@ enum tap_dance_codes {
     [_FNX_LAYER]   = LAYOUT_60_ansi(/* Holding FN2 => FN1 at the same time */
         KC_AP2_USB, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, KC_AP2_BT_UNPAIR, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, KC_AP_WIFE, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, KC_AP_WIN, _______, _______, _______, _______, _______, KC_AP_LIN, _______, _______, _______,
+        KC_AP_NO_ESC, _______, _______, KC_AP_WIN, _______, _______, _______, _______, _______, KC_AP_LIN, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, KC_AP_MAC, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______),
 };
@@ -212,6 +220,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         rgb_matrix_set_speed(current_speed + 51);
       }
       return false; // Skip all further processing of this key
+    case KC_AP_NO_ESC:
+      if (record->event.pressed) {
+        default_layer_set(1 << _NO_ESC_LAYER);
+      }
+      return false;
     case KC_AP_WIN:
       if (record->event.pressed) {
         default_layer_set(1 << _BASE_LAYER);
