8f44a065ce11671df39d8038a48b7e010c044330
Darkhan Kubigenov <darkhanu@gmail.com>
2024-03-16 22:43:38 +0000

    Fix mouse button

--- keyboards/sofle/keymaps/kdarkhan/keymap.c
+++ keyboards/sofle/keymaps/kdarkhan/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [1] = LAYOUT(
   _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,   KC_F5,                        KC_F6,      KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
-  KC_UNDO,  KC_WREF,  KC_BTN3,  KC_MS_U,  KC_BTN2, _______,                      KC_CIRC,    KC_LBRC,  KC_RBRC,  KC_LCBR,  KC_RCBR,   KC_F12,
+  KC_UNDO,  KC_WREF,  KC_BTN1,  KC_MS_U,  KC_BTN2, _______,                      KC_CIRC,    KC_LBRC,  KC_RBRC,  KC_LCBR,  KC_RCBR,   KC_F12,
   KC_CAPS,  KC_WHOM,  KC_MS_L,  KC_MS_D,  KC_MS_R, _______,                      KC_LEFT,    KC_DOWN,  KC_UP,    KC_RGHT,  KC_DEL,   KC_BSPC,
   KC_WBAK,  KC_WFWD,  KC_WH_D,  _______,  KC_WH_U, _______, _______,    _______, KC_ASTR,    KC_LPRN,  KC_RPRN,  KC_AMPR,  KC_EQL,  _______,
                 KC_MPRV,        KC_MPLY,  KC_MNXT, _______, KC_DEL,     _______, _______, _______, KC_HOME, KC_END
