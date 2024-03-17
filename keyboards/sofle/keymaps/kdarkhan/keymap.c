f05e16b20d4f86c51e57aac981a6bfafd15cbd57
Darkhan Kubigenov <darkhanu@gmail.com>
2024-03-17 00:58:33 +0000

    More keymap adjusts

--- keyboards/sofle/keymaps/kdarkhan/keymap.c
+++ keyboards/sofle/keymaps/kdarkhan/keymap.c
@@ -25,6 +25,23 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [3] = { ENCODER_CCW_CW(_______, _______),           ENCODER_CCW_CW(_______, _______) },
     [4] = { ENCODER_CCW_CW(_______, _______),           ENCODER_CCW_CW(_______, _______) }
 };
+#else
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    } else { /* Second encoder */
+        if (clockwise) {
+            tap_code(KC_RGHT);
+        } else {
+            tap_code(KC_LEFT);
+        }
+    }
+    return false;
+}
 #endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -46,10 +63,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [0] = LAYOUT(
   KC_GRV,         KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
-  LT(1, KC_TAB),  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  LT(1, KC_BSLS),
+  LCTL_T(KC_TAB),  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  RCTL_T(KC_BSLS),
   LSFT_T(KC_ESC), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  RSFT_T(KC_QUOT),
-  KC_LALT,        KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     _______, KC_N,    KC_M,    KC_COMM,  KC_DOT, KC_SLSH,  KC_RALT,
-                          KC_MENU, KC_LGUI, KC_LCTL, KC_SPACE, KC_BSPC,    KC_RGUI, KC_ENT,  KC_RCTL, KC_RALT, MO(2)
+  KC_LGUI,        KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     _______, KC_N,    KC_M,    KC_COMM,  KC_DOT, KC_SLSH,  KC_RGUI,
+                          KC_MENU, KC_LALT, MO(1), KC_SPACE, KC_BSPC,    KC_RALT, KC_ENT,  KC_BSPC, KC_RALT, MO(2)
 ),
 /*
  * CAPS HELD
