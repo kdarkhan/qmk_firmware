5132b1f73f197b521ade3b9284c873f32c5ba256
Darkhan Kubigenov <darkhanu@gmail.com>
2024-03-19 23:17:29 +0000

    Closer to default keymap

--- keyboards/sofle/keymaps/kdarkhan/keymap.c
+++ keyboards/sofle/keymaps/kdarkhan/keymap.c
@@ -69,11 +69,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [0] = LAYOUT(
-  KC_GRV,         KC_1,          KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,             KC_MINS,
-  LT(1, KC_TAB),  KC_Q,          KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,             LT(1, KC_BSLS),
-  LSFT_T(KC_ESC), LCTL_T(KC_A),  KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    RCTL_T(KC_SCLN),  RSFT_T(KC_QUOT),
-  KC_LALT,        KC_Z,          KC_X,    KC_C,    KC_V,    KC_B,     KC_MUTE,   _______, KC_N,    KC_M,    KC_COMM,  KC_DOT, KC_SLSH,          KC_RALT,
-                                 KC_MENU, KC_LALT, MO(1),   KC_SPACE, KC_LGUI,   KC_BSPC, KC_ENT,  MO(1),   KC_MENU, MO(2)
+  KC_GRV,         KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,      KC_MINS,
+  LT(1, KC_TAB),  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,      LT(1, KC_BSLS),
+  LT(2, KC_ESC),  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,   LT(2, KC_BSPC),
+  KC_LALT,        KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,     KC_MUTE,   _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_RALT,
+                          KC_LGUI, KC_LALT, KC_LCTL, KC_ENT,   MO(1),     MO(1),   KC_SPC,   KC_RCTL, KC_RALT, KC_RGUI
 ),
 /*
  * CAPS HELD
@@ -114,11 +114,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [2] = LAYOUT(
-  _______,  _______,  _______,  _______,  _______, _______,                      _______,    _______,    _______,    _______,    _______,  _______,
-  KC_TAB,  KC_INS,    KC_APP,   _______,  _______, _______,                      _______,    _______,    _______,    _______, _______,  KC_BSLS,
-  KC_LSFT,  KC_PSCR,  _______,  _______,  _______, _______,                      _______,    _______,    _______,    _______,    _______,  KC_RSFT,
-  _______,  _______,  _______,  _______,  _______, _______, _______,      _______,_______,    _______, _______,  _______, _______,  _______,
-                _______,_______,_______,_______, _______,      _______,  _______, _______, _______, _______
+  _______,  _______,  _______,  _______,  _______, _______,                      _______,    _______,  _______,  _______, _______,  _______,
+  KC_TAB,  KC_INS,    KC_APP,   _______,  _______, _______,                      _______,    _______,  _______,  _______, _______,  KC_BSLS,
+  KC_LSFT,  KC_PSCR,  _______,  _______,  _______, _______,                      KC_LEFT,    KC_DOWN,  KC_UP,    KC_RGHT, _______,  KC_DEL,
+  _______,  _______,  _______,  _______,  _______, _______, _______,    _______, _______,    _______,  _______,  _______, _______,  _______,
+                      _______,  _______,  _______, _______, _______,    _______, _______,    _______,  _______,  _______
 ),
 /* RAISE
  * ,----------------------------------------.                    ,-----------------------------------------.
