075e4bf51191510e83126c041d5090aefc069d41
Darkhan Kubigenov <darkhanu@gmail.com>
2025-07-28 01:13:25 +0100

    Attempt homerow mods

--- keyboards/sofle/keymaps/kdarkhan/config.h
+++ keyboards/sofle/keymaps/kdarkhan/config.h
@@ -3,11 +3,20 @@
 #define ONESHOT_TAP_TOGGLE 2
 #define ONESHOT_TIMEOUT 5000
 
-//#define ENCODER_MAP_KEY_DELAY 50
+// #define ENCODER_MAP_KEY_DELAY 50
 #define EE_HANDS
-//#define OLED_UPDATE_INTERVAL 200
+// #define OLED_UPDATE_INTERVAL 200
 
 #ifdef OLED_ENABLE
-    #define OLED_TIMEOUT 120000
-    #define OLED_UPDATE_INTERVAL 100
+#  define OLED_TIMEOUT 120000
+#  define OLED_UPDATE_INTERVAL 100
 #endif
+
+// homerow mods https://precondition.github.io/home-row-mods
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 100
+
+#define BILATERAL_COMBINATIONS 500
